1b4b1278a4b0bec09f75d095c53c10d52afa49ae
SunTianTian2333 <2669516820@qq.com>
2022-04-18 00:01:49 -0700

    hw02 is done

---
+++ hw02/parsons_probs/count_until_larger.py
@@ -0,0 +1,36 @@
+def count_until_larger(num):
+    """
+    Complete the function count_until_larger that takes in a positive integer num.
+    count_until_larger examines the rightmost digit and counts digits from right to
+    left until it encounters a digit larger than the rightmost digit, then returns that count.
+
+    >>> count_until_larger(117) # .Case 1
+    -1
+    >>> count_until_larger(8117) # .Case 2
+    3
+    >>> count_until_larger(9118117) # .Case 3
+    3
+    >>> count_until_larger(8777)  # .Case 4
+    3
+    >>> count_until_larger(22) # .Case 5
+    -1
+    >>> count_until_larger(0) # .Case 6
+    -1
+    """
+    "*** YOUR CODE HERE ***"
+    k=num%10
+    i=1
+    while num:
+          num=num//10
+          temp=num%10
+          if temp>k:
+             return i
+          i+=1
+    return -1
+             
+
+
+
+
+
+
